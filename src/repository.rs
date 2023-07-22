ffb2298a997a0baffdd6d68001418ca29e9d580d
Simon Wikstrand <simon@wikstrand.dev>
2023-07-22 17:52:02 +0200

    improved tests and security rules

--- src/repository.rs
+++ src/repository.rs
@@ -1,4 +1,5 @@
 use crate::donation::donation::Donation;
+use crate::donation::generate_url;
 use scrypto::prelude::*;
 
 #[derive(ScryptoSbor, NonFungibleData, Clone)]
@@ -15,10 +16,12 @@ pub(crate) struct TrophyData {
 mod repository {
     enable_method_auth! {
         roles {
+            admin => updatable_by: [OWNER];
             trophy_minter => updatable_by: [OWNER];
         },
         methods {
             new_donation_component => PUBLIC;
+            update_base_path => restrict_to: [admin];
         }
     }
 
@@ -26,27 +29,28 @@ mod repository {
         // NFT resource address.
         trophy_resource_manager: ResourceManager,
 
+        // Owner badge is given to the owner of the component.
+        owner_badge_manager: ResourceManager,
+
         // Badge for being able to mint trophies.
         minter_badge_manager: ResourceManager,
-
-        // Base path, e.g https://backeum.com/nft_image
-        base_path: String,
     }
 
     impl Repository {
         pub fn new(base_path: String) -> (Global<Repository>, Bucket) {
+            let (address_reservation, component_address) =
+                Runtime::allocate_component_address(Runtime::blueprint_id());
+
             // Creating an admin badge for the admin role
             let owner_badge = ResourceBuilder::new_fungible(OwnerRole::None)
                 .divisibility(DIVISIBILITY_NONE)
                 .metadata(metadata!(
                     init {
                         "name" => "Owner Badge", locked;
-                        "description" => "Backeum trophies owner badge", locked;
+                        "description" => "Repository owner badge", locked;
                     }
                 ))
                 .mint_initial_supply(1);
-
-            let package_address = Runtime::package_address();
 
             // Creating an admin badge for the admin role
             let minter_badge_manager = ResourceBuilder::new_fungible(OwnerRole::None)
@@ -54,14 +58,14 @@ mod repository {
                 .metadata(metadata!(
                     init {
                         "name" => "Trophies Minter", locked;
-                        "description" => "Grants authorization to mint NFTs from Backeum repository", locked;
+                        "description" => "Grants authorization to mint NFs from repository", locked;
                     }
                 ))
-                .mint_roles(mint_roles!{
-                    minter => rule!(require(package_of_direct_caller(package_address)));
+                .mint_roles(mint_roles! {
+                    minter => rule!(require(global_caller(component_address)));
                     minter_updater => rule!(deny_all);
                 })
-                .withdraw_roles(withdraw_roles!{
+                .withdraw_roles(withdraw_roles! {
                     withdrawer => rule!(deny_all);
                     withdrawer_updater => rule!(deny_all);
                 })
@@ -69,47 +73,90 @@ mod repository {
 
             let trophy_resource_manager = ResourceBuilder::new_ruid_non_fungible::<TrophyData>(OwnerRole::None)
                 .metadata(metadata!(
+                    roles {
+                        metadata_setter => rule!(require(owner_badge.resource_address()));
+                        metadata_setter_updater => rule!(deny_all);
+                        metadata_locker => rule!(deny_all);
+                        metadata_locker_updater => rule!(deny_all);
+                    },
                     init {
                         "name" => "Backeum Trophies", locked;
-                        "description" => "Backeum trophies celebrates the patronage of its holder with donations to individual Backeum creators. A unique symbol of support for the community, it's a vibrant testament to encouragement.", locked;
+                        "description" => "Backeum trophies celebrates the patronage of its holder with donations to individual Backeum creators. A unique symbol of support for the community, it's a vibrant testament to financial encouragement.", locked;
+                        "domain" => base_path, updatable;
                     }
                 ))
                 .withdraw_roles(withdraw_roles!(
-                    withdrawer => rule!(require(package_of_direct_caller(package_address)));
+                    withdrawer => rule!(require(minter_badge_manager.address()));
                     withdrawer_updater => rule!(deny_all);
                 ))
                 .mint_roles(mint_roles!(
-                    minter => rule!(require(package_of_direct_caller(package_address)));
+                    minter => rule!(require(minter_badge_manager.address()));
                     minter_updater => rule!(deny_all);
                 ))
                 .non_fungible_data_update_roles(non_fungible_data_update_roles!(
-                    non_fungible_data_updater => rule!(require(package_of_direct_caller(package_address)));
+                    non_fungible_data_updater => rule!(require(minter_badge_manager.address()) || require(owner_badge.resource_address()));
                     non_fungible_data_updater_updater => rule!(deny_all);
                 ))
                 .create_with_no_initial_supply();
 
             let component = Self {
                 trophy_resource_manager,
+                owner_badge_manager: owner_badge.resource_manager(),
                 minter_badge_manager,
-                base_path,
             }
             .instantiate()
-            .prepare_to_globalize(OwnerRole::None)
+            .prepare_to_globalize(OwnerRole::Fixed(rule!(require(
+                owner_badge.resource_address()
+            ))))
             .roles(roles! {
                 trophy_minter => rule!(require(minter_badge_manager.address()));
+                admin => rule!(require(owner_badge.resource_address()));
             })
+            .with_address(address_reservation)
             .globalize();
 
             (component, owner_badge)
         }
 
-        // new_donation_component sets up a new donation contract for a user, and give that contract
+        // new_donation_component sets up a new donation component for a user, and give that contract
         // a mint badge that allows for it to create and update trophies. By going through Repository
         // for instantiation we can ensure that the mint badge is only given to a contract that is
         // made by Backeum.
         pub fn new_donation_component(&mut self) -> (Global<Donation>, Bucket) {
             let mint_badge = self.minter_badge_manager.mint(1);
             Donation::new(self.trophy_resource_manager, mint_badge, "".to_string())
+        }
+
+        // update_base_path updates the base path for each trophy.
+        pub fn update_base_path(
+            &mut self,
+            new_base_path: String,
+            update_nft_ids: Vec<NonFungibleLocalId>,
+        ) {
+            self.trophy_resource_manager
+                .set_metadata("domain", new_base_path.clone());
+
+            for nft_id in update_nft_ids {
+                // Get data from the Trophy data based on NF id.
+                let mut data: TrophyData =
+                    self.trophy_resource_manager.get_non_fungible_data(&nft_id);
+
+                // Generate new image url.
+                data.key_image_url = generate_url(
+                    new_base_path.to_string(),
+                    data.donated,
+                    data.created,
+                    nft_id.to_string(),
+                    data.user_identity,
+                );
+
+                // Update image url.
+                self.trophy_resource_manager.update_non_fungible_data(
+                    &nft_id,
+                    "key_image_url",
+                    data.key_image_url,
+                );
+            }
         }
     }
 }
--- tests/repository.rs
+++ tests/repository.rs
@@ -1,75 +1,231 @@
 use scrypto::prelude::*;
 use scrypto_unit::*;
-use transaction::builder::ManifestBuilder;
+use transaction::{
+    builder::ManifestBuilder, prelude::Secp256k1PrivateKey, prelude::Secp256k1PublicKey,
+};
 
-#[test]
-fn test_repository_create_donation_contract() {
-    // Setup the environment
-    let mut test_runner = TestRunner::builder().without_trace().build();
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    // Create an owner account
-    let (public_key, _private_key, account) = test_runner.new_allocated_account();
+    struct TestAccount {
+        public_key: Secp256k1PublicKey,
+        _private_key: Secp256k1PrivateKey,
+        wallet_address: ComponentAddress,
+    }
 
-    // Publish package
-    let package_address = test_runner.compile_and_publish(this_package!());
+    impl TestAccount {
+        fn new(test_runner: &mut TestRunner) -> Self {
+            let (public_key, _private_key, component_address) = test_runner.new_allocated_account();
+            Self {
+                public_key,
+                _private_key,
+                wallet_address: component_address,
+            }
+        }
+    }
 
-    // Test the `instantiate_hello` function.
-    let manifest = ManifestBuilder::new()
-        .call_function(
-            package_address,
-            "Repository",
-            "new",
-            manifest_args!("https://localhost:8080/nft_image"),
-        )
-        .deposit_batch(account)
-        .build();
+    struct TestSetup {
+        test_runner: TestRunner,
+        package_address: PackageAddress,
+        repository_component: ComponentAddress,
+        owner_account: TestAccount,
+        owner_badge_resource_address: ResourceAddress,
+        trophy_resource_address: ResourceAddress,
+    }
 
-    let receipt = test_runner.execute_manifest_ignoring_fee(
-        manifest,
-        vec![NonFungibleGlobalId::from_public_key(&public_key)],
-    );
+    impl TestSetup {
+        fn new() -> Self {
+            let mut test_runner = TestRunner::builder().without_trace().build();
 
-    let repository_component = receipt.expect_commit(true).new_component_addresses()[0];
-    let donor_badge_address = receipt.expect_commit(true).new_resource_addresses()[0];
-    let resources = test_runner.get_component_resources(repository_component);
+            // Create an owner account
+            let owner_account = TestAccount::new(&mut test_runner);
 
-    println!("resources: {:?}", resources);
-    println!("donor_badge_address: {:?}", donor_badge_address);
+            // Publish package
+            let package_address = test_runner.compile_and_publish(this_package!());
 
-    // Create an owner account
-    let (public_key2, _, account2) = test_runner.new_allocated_account();
+            // Test the repository component via the new function.
+            let manifest = ManifestBuilder::new()
+                .call_function(
+                    package_address,
+                    "Repository",
+                    "new",
+                    manifest_args!("https://localhost:8080/nft_image"),
+                )
+                .deposit_batch(owner_account.wallet_address)
+                .build();
 
-    let manifest2 = ManifestBuilder::new()
-        .call_method(
-            repository_component,
-            "new_donation_component",
-            manifest_args!(),
-        )
-        .deposit_batch(account2)
-        .build();
+            // Execute the manifest.
+            let receipt = test_runner.execute_manifest_ignoring_fee(
+                manifest,
+                vec![NonFungibleGlobalId::from_public_key(
+                    &owner_account.public_key,
+                )],
+            );
 
-    let receipt2 = test_runner.execute_manifest_ignoring_fee(
-        manifest2,
-        vec![NonFungibleGlobalId::from_public_key(&public_key2)],
-    );
+            // Get the repository component address.
+            let repository_component = receipt.expect_commit(true).new_component_addresses()[0];
 
-    let donation_component = receipt2.expect_commit(true).new_component_addresses()[0];
+            // Get the owner badge resource address.
+            let owner_badge_resource_address =
+                receipt.expect_commit(true).new_resource_addresses()[0];
+            let trophy_resource_address = receipt.expect_commit(true).new_resource_addresses()[2];
+            Self {
+                test_runner,
+                package_address,
+                repository_component,
+                owner_account,
+                owner_badge_resource_address,
+                trophy_resource_address,
+            }
+        }
+    }
 
-    // Create an owner account
-    let (public_key3, _, account3) = test_runner.new_allocated_account();
+    #[test]
+    fn repository_test() {
+        TestSetup::new();
+    }
 
-    let manifest3 = ManifestBuilder::new()
-        .withdraw_from_account(account3, RADIX_TOKEN, dec!(100))
-        .take_from_worktop(RADIX_TOKEN, dec!(100), "donation_amount")
-        .call_method_with_name_lookup(donation_component, "donate_mint", |lookup| {
-            (lookup.bucket("donation_amount"),)
-        })
-        .deposit_batch(account3)
-        .build();
+    #[test]
+    fn repository_update_base_path() {
+        let mut base = TestSetup::new();
 
-    let receipt3 = test_runner.execute_manifest_ignoring_fee(
-        manifest3,
-        vec![NonFungibleGlobalId::from_public_key(&public_key3)],
-    );
-    receipt3.expect_commit(true);
+        // Create an component admin account
+        let admin_account = TestAccount::new(&mut base.test_runner);
+        // Create donation account
+        let donation_account = TestAccount::new(&mut base.test_runner);
+
+        // Create a donation component
+        let manifest1 = ManifestBuilder::new()
+            .call_method(
+                base.repository_component,
+                "new_donation_component",
+                manifest_args!(),
+            )
+            .deposit_batch(admin_account.wallet_address)
+            .build();
+
+        // Execute it
+        let receipt1 = base.test_runner.execute_manifest_ignoring_fee(
+            manifest1,
+            vec![NonFungibleGlobalId::from_public_key(
+                &admin_account.public_key,
+            )],
+        );
+
+        // Get the resource address
+        let donation_component = receipt1.expect_commit(true).new_component_addresses()[0];
+
+        // Donate and mint trophy
+        let manifest2 = ManifestBuilder::new()
+            .withdraw_from_account(donation_account.wallet_address, RADIX_TOKEN, dec!(100))
+            .take_from_worktop(RADIX_TOKEN, dec!(100), "donation_amount")
+            .call_method_with_name_lookup(donation_component, "donate_mint", |lookup| {
+                (lookup.bucket("donation_amount"),)
+            })
+            .take_all_from_worktop(base.trophy_resource_address, "trophy")
+            .try_deposit_or_abort(donation_account.wallet_address, "trophy")
+            .build();
+
+        let receipt2 = base.test_runner.execute_manifest_ignoring_fee(
+            manifest2,
+            vec![NonFungibleGlobalId::from_public_key(
+                &donation_account.public_key,
+            )],
+        );
+
+        receipt2.expect_commit_success();
+        assert_eq!(
+            base.test_runner.account_balance(
+                donation_account.wallet_address,
+                base.trophy_resource_address
+            ),
+            Some(dec!(1))
+        );
+        assert_eq!(
+            base.test_runner
+                .account_balance(donation_account.wallet_address, RADIX_TOKEN),
+            Some(dec!(9900))
+        );
+
+        // Get the Non fungible id out of the stack
+        let trophy_vault = base.test_runner.get_component_vaults(
+            donation_account.wallet_address,
+            base.trophy_resource_address,
+        );
+        let vault_content = base
+            .test_runner
+            .inspect_non_fungible_vault(trophy_vault[0])
+            .unwrap();
+        assert_eq!(vault_content.0, dec!(1));
+        let trophy_id = vault_content.1.unwrap();
+
+        // Test rejection to update the base path with a donation account
+        let manifest3 = ManifestBuilder::new()
+            .create_proof_from_account_of_amount(
+                donation_account.wallet_address,
+                base.owner_badge_resource_address,
+                dec!(1),
+            )
+            .call_method(
+                base.repository_component,
+                "update_base_path",
+                manifest_args!("https://some_other_url/nft_image", vec![trophy_id.clone()]),
+            )
+            .build();
+
+        let receipt3 = base.test_runner.execute_manifest_ignoring_fee(
+            manifest3,
+            vec![NonFungibleGlobalId::from_public_key(
+                &donation_account.public_key,
+            )],
+        );
+
+        receipt3.expect_commit_failure();
+
+        // Test rejection to update the base path with a non owner account
+        let manifest4 = ManifestBuilder::new()
+            .create_proof_from_account_of_amount(
+                admin_account.wallet_address,
+                base.owner_badge_resource_address,
+                dec!(1),
+            )
+            .call_method(
+                base.repository_component,
+                "update_base_path",
+                manifest_args!("https://some_other_url/nft_image", vec![trophy_id.clone()]),
+            )
+            .build();
+
+        let receipt4 = base.test_runner.execute_manifest_ignoring_fee(
+            manifest4,
+            vec![NonFungibleGlobalId::from_public_key(
+                &admin_account.public_key,
+            )],
+        );
+
+        receipt4.expect_commit_failure();
+
+        // Test rejection to update the base path with a non owner account
+        let manifest5 = ManifestBuilder::new()
+            .create_proof_from_account_of_amount(
+                base.owner_account.wallet_address,
+                base.owner_badge_resource_address,
+                dec!(1),
+            )
+            .call_method(
+                base.repository_component,
+                "update_base_path",
+                manifest_args!("https://some_other_url/nft_image", vec![trophy_id.clone()]),
+            )
+            .build();
+
+        let receipt5 = base.test_runner.execute_manifest_ignoring_fee(
+            manifest5,
+            vec![NonFungibleGlobalId::from_public_key(
+                &base.owner_account.public_key,
+            )],
+        );
+        receipt5.expect_commit_success();
+    }
 }
